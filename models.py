918bf4498636b8730d3c8ff06ef4cf40150aac02
Eli Pandolfo <epandolf@ucsc.edu>
2019-09-03 15:47:42 -0700

    endowment configurable

--- models.py
+++ models.py
@@ -16,13 +16,13 @@ class Constants(BaseConstants):
     num_rounds = 10 # multiples of 5
     players_per_group = 5
     preferences = [15, 30, 50, 70, 95]
-    endowment = c(100) # initial endowment for each player
 
 class Subsession(BaseSubsession):
     def creating_session(self):
         if self.round_number == 1:
             self.group_randomly()
-            self.participant.payoff += Constants.endowment
+            for p in self.get_players():
+                p.participant.payoff += c(self.session.config['endowment'])
         else:
             self.group_like_round(1)
         for p in self.get_players():
